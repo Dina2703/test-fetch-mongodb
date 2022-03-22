aef15c0a611c69cf274039a10e690fb96a5328e6
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-22 14:11:53 -0400

    removed all mongodb data to test deployment

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
-import clientPromise from "../lib/mongodb";
 
-export default function Home({ movies }) {
+export default function Home() {
   // console.log(movies);
   return (
     <div>
@@ -12,25 +11,8 @@ export default function Home({ movies }) {
       </Head>
       <h3>hello</h3>
       <div>
-        <ul>
-          {movies &&
-            movies.map((movie) => <li key={movie._id}>{movie.title}</li>)}
-        </ul>
+        <h3>Hello</h3>
       </div>
     </div>
   );
-}
-
-export async function getServerSideProps(ctx) {
-  const client = await clientPromise;
-  const db = client.db("sample_mflix");
-  const data = await db
-    .collection("movies")
-    .find({ year: 2014, "imdb.rating": { $gt: 8 } })
-    .limit(15)
-    .toArray();
-  const movies = JSON.parse(JSON.stringify(data));
-  return {
-    props: { movies },
-  };
 }
